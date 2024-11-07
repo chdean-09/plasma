9c33b35f90740564dfb92b58c7a7065d1e0c7f65
chdean-09 <chadandrada20@gmail.com>
2024-11-07 20:03:13 +0800

    initialize plasmo framework

---
+++ src/popup.tsx
@@ -0,0 +1,13 @@
+import { CountButton } from "~features/count-button"
+
+import "~style.css"
+
+function IndexPopup() {
+  return (
+    <div className="flex flex-col items-center justify-center h-16 w-40">
+      <CountButton />
+    </div>
+  )
+}
+
+export default IndexPopup
